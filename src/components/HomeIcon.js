9ebaf3f2112cfb1ddef26176fe62033b529b98f8
Hong Yong <hongyongwhy@users.noreply.github.com>
2020-02-27 19:22:49 +0800

    Added home page

---
+++ src/components/HomeIcon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./HomeIcon.css";
+
+class HomeIcon extends Component {
+    render() {
+        return (
+            <div id="home-icon">
+                <p>Home</p>
+            </div>
+        );
+    }
+}
+
+export default HomeIcon;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import HomeIcon from '../components/HomeIcon';
+import "./Home.css";
+import { ReactComponent as SearchIcon} from "./assets/search.svg";
+import { ReactComponent as HomePlaceholder} from "./assets/home-placeholder.svg";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                <HomeIcon />
+                <p id="welcome-msg" className="center">Hey USERNAME!</p>
+                <h1 id="title" className="center">Welcome to Showtime</h1>
+                <div className="input-holder center">
+                    <SearchIcon className="search-icon"/>
+                    <input id="course-search" className="center" placeholder="Type the code or title of a course"/>
+                </div>
+                <HomePlaceholder className="home-placeholder center" />
             </div>
         );
     }
